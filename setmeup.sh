12ddefbc04c96d96514b0b17b07e586a503a50b2
Erick Rojas <rojas_erick@hotmail.com>
2024-12-11 11:09:21 -0500

    Update setmeup.sh
    - Add rectangle-app
    - Add handbrake
    - Add google chrome
    - Add ms-edge
    - Add firefox-developer
    - Copy vscode keybindings.json to $HOME/Library/Application\ Support/Code/User
    - Install useful vscode extensions

--- setmeup.sh
+++ setmeup.sh
@@ -8,6 +8,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install MacOS applications
+brew install --cask google-chrome
+brew install --cask microsoft-edge
+brew install --cask firefox@developer-edition
+brew install --cask rectangle
 brew install --cask maccy
 brew install --cask alt-tab
 brew install --cask keycastr
@@ -21,6 +25,7 @@ brew install --cask krita
 brew install ollama
 brew install --cask spotify
 brew install --cask vlc
+brew install handbrake
 
 # Install CLI applications and CLI dev tools
 brew install openjdk@21
@@ -39,6 +44,8 @@ pyenv install 3.13
 
 # Install LazyVim
 brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-dejavu-sans-mono-nerd-font
+brew install --cask font-arimo-nerd-font
 brew install fd
 brew install lazygit
 brew install ripgrep
@@ -55,6 +62,18 @@ rm -rf ~/.config/nvim/.git
 # Configure vscode
 mkdir -p $HOME/Library/Application\ Support/Code/User
 cp ./vscode/settings.json $HOME/Library/Application\ Support/Code/User
+cp ./vscode/keybindings.json $HOME/Library/Application\ Support/Code/User
+
+## Install vscode extension
+code --install-extension ms-python.python
+code --install-extension ms-python.black-formatter
+code --install-extension eamodio.gitlens
+code --install-extension ms-vscode.live-server
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension ms-toolsai.jupyter
+code --install-extension rangav.vscode-thunder-client
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 
 # Copy tmux configuration
 cp .tmux.conf $HOME/.tmux.conf
